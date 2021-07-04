9893ac182cfe34c3f7ddc57341211fec0f23ba9d
Jennifer Daly <jenniferdaly94@gmail.com>
2021-07-05 00:54:02 +0100

    adding react-google-maps/api

--- dublinBus/dublinBusHybrid/static/js/index.js
+++ dublinBus/dublinBusHybrid/static/js/index.js
@@ -1,5 +1,6 @@
-import ComponentOne from './component-one'
-import ComponentTwo from './component-two'
+import ComponentOne from './component-one';
+import ComponentTwo from './component-two';
+import Maps from './map';
 
 ComponentOne();
 ComponentTwo();

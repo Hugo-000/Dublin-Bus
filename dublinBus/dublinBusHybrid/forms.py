de6f14cde55a7704e8260b1c2fba30d964f7d4d8
Jennifer Daly <jenniferdaly94@gmail.com>
2021-07-08 23:22:17 +0100

    validation and error handelign for forms
    Updated the way the forms data was cleaned. Changed it from being in the
    views.py classes and moved it into a function in the forms.py file.
    
    Additionally implemented the form to inform the user what the issues may
    be if the form fails. ie required field or incorrect data.
    
    Started implementing the results page for the journey planner. Currently
    it extends the journey planner page complete with the form.

--- dublinBus/dublinBusHybrid/forms.py
+++ dublinBus/dublinBusHybrid/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+import datetime
+from scrapper.models import Stops
+
 class JourneyPlannerForm(forms.Form):
     start_point = forms.CharField(
         label='Starting Point', 
@@ -12,4 +15,27 @@ class JourneyPlannerForm(forms.Form):
         #     'data-url': "/dublinbushybrid/stopID_autocomplete/"
         )
     destination_point = forms.CharField(label='Destination Point', max_length=100)
-    travel_date = forms.DateField(label='Please choose a date within the next 7 days')
+    travel_date = forms.DateField(label='Please choose a date within the next 7 days') #, widget=forms.DateInput(format = '%d/%m/%Y'), input_formats=settings.DATE_INPUT_FORMATS)
+
+    def clean(self):
+        cd = self.cleaned_data
+        start = cd.get('start_point')
+        destination = cd.get('destination_point')
+
+        if len(Stops.objects.filter(stop_name=start)) == 0:
+            self.add_error('start_point', 'No starting stop with this name')
+
+        if len(Stops.objects.filter(stop_name=destination)) == 0:
+            self.add_error('destination_point', 'No destination stop with this name')
+
+        travel_date = cd.get('travel_date')
+        today = datetime.date.today()
+
+        if travel_date and travel_date < today:
+            self.add_error('travel_date', 'Invalid date - date of travl is in past')
+
+        # Check if a date is in the allowed range (+4 weeks from today).
+        if travel_date and travel_date > today + datetime.timedelta(days=7):
+            self.add_error('travel_date', 'Invalid date - date of travel is too far in the future')
+
+        return cd
--- dublinBus/dublinBusHybrid/views.py
+++ dublinBus/dublinBusHybrid/views.py
@@ -11,6 +11,8 @@ from .forms import JourneyPlannerForm
 
 from scrapper.models import Stops
 
+import datetime
+
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
@@ -20,39 +22,15 @@ class JourneyPlanner(View):
     #     return render(request, 'journey-planner.html')
 
     def get(self, request):
-        s = Stops.objects.filter(stop_name='Parnell Square West')
-        print("HELLO", s)
-        return render(request, 'journey-planner.html', {'form': JourneyPlannerForm()})
-
-    # def post(self, request, *args, **kwargs):
-    #     return HttpResponse('This is POST request') 
+        return render(request, 'journeyPlanner.html', {'form': JourneyPlannerForm()})
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        s = Stops.objects.filter(stop_name='Parnell Square West')
-        print("HELLO", s)
         form = JourneyPlannerForm(request.POST)
-        print(form.errors)
-        # check whether it's valid:
-        if form.is_valid():
-            start_point = form.cleaned_data['start_point']
-            print('Start Point', start_point)
-            destination_point = form.cleaned_data['destination_point']
-            print('Destination', destination_point)
-            s = Stops.objects.filter(stop_name=start_point)
-            d = Stops.objects.filter(stop_name=destination_point)
-            print("HELLO", s)
-            if s.count() == 0:		
-                raise Http404("No starting stop with this name")
 
-            if d.count() == 0:
-                raise Http404("No destination stop with this name")
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponse('thanks')
+        if form.is_valid():
+            return render(request, 'journeyPlanner/map.html', { 'form': form })
         else:
-            return render(request, 'journey-planner.html', {'form': form})
+            return render(request, 'journeyPlanner.html', { 'form': form })
 
 
     

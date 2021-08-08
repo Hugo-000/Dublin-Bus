569b79341b959247ab21978a67aa5721de2fd6f9
chowsychoch <cho.cho@ucdconnect.ie>
2021-08-08 12:06:59 +0800

    test case merge

--- dublinBus/dublinBusHybrid/tests/test_views.py
+++ dublinBus/dublinBusHybrid/tests/test_views.py
@@ -1,11 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
-<<<<<<< HEAD
 from dublinBusHybrid.views import JourneyPlanner, BusRoutes, Index
 from dublinBusHybrid.forms import JourneyPlannerForm
-=======
-from dublinBusHybrid.views import JourneyPlanner
->>>>>>> f31fa0ab5df1369400d9b37bccc62382764f8e8a
 import os
 import sys
 
@@ -14,13 +10,9 @@ sys.path.append(BASE_DIR)
 print("test base", BASE_DIR)
 
 from django.test.runner import DiscoverRunner
-
-
 # myapp.tests.py
 from django.test import TestCase, SimpleTestCase
 
-
-<<<<<<< HEAD
 class TestIndexView(TestCase):
     def test_resolve_to_index(self):
         """test the url works fine"""
@@ -75,45 +67,3 @@ class TestIndexView(TestCase):
         print(response.status_code)
         print(response.content)
         self.assertEqual(response.resolver_match.func.__name__, BusRoutes.post(request=response))
-=======
-class DbTestCase(TestCase):
-    """Does something with the DB."""
-    def setup_databases(self, **kwargs):
-        pass
-
-    def teardown_databases(self, old_config, **kwargs):
-        pass
-
-
-
-class NoDbTestCase(SimpleTestCase):
-    """Does something with the DB."""
-    def setup_databases(self, **kwargs):
-        pass
-
-    def teardown_databases(self, old_config, **kwargs):
-        pass
-
-class NoDbTestRunner(DiscoverRunner):
-    """ A test runner to test without database creation/deletion """
-
-    def setup_databases(self, **kwargs):
-        pass
-
-    def teardown_databases(self, old_config, **kwargs):
-        pass
-
-
-
-class TestIndexView(TestCase):
-
-    def test_resolve_to_test_get_index(self):
-        """test the url works fine"""
-        # resolve root path
-        found = resolve('/')
-
-        # check function name is equal
-        self.assertEqual(found.func.__name__, JourneyPlanner.as_view( ).__name__)
-
-
->>>>>>> f31fa0ab5df1369400d9b37bccc62382764f8e8a

c092212a502b7f3bcaf23ad9c81c5552cc733175
Jennifer Daly <jenniferdaly94@gmail.com>
2021-08-18 22:44:51 +0100

    code to turn on if the crontab information needs to be logged for testing purposes

---
+++ dublinBus/log/debug.md
@@ -0,0 +1,3 @@
+# Logs
+
+This folder is for holding log files. We need this file in order to add the log folder to the git repo, while the files can be ignored (django-cron will create them) it will not create the folder if it doesn't exist.
--- dublinBus/scrapper/cron.py
+++ dublinBus/scrapper/cron.py
@@ -13,12 +13,14 @@ def my_scheduled_job():
   os.system("open /Users/chowsy/PycharmProjects/G1_RP_Dublin-Bus-App/backend/base/hello.txt")
 
 def scheduling_curr_weather():
+  print("testing current weather is running")
   BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+  print("Base_DIR", BASE_DIR)
   sys.path.append(BASE_DIR)
   print(BASE_DIR)
   #result = subprocess.run(["source " + BASE_DIR + "/myenv/bin/activate" +";" + "python3 "+ "/Users/chowsy/PycharmProjects/G1_RP_Dublin-Bus-App/backend/base/scrapper/current_weather.py"],shell=True)
   result = subprocess.run(["source " + BASE_DIR + "/myenv/bin/activate" +";" + "python3 "+ BASE_DIR + "/dublinBus/scrapper/current_weather.py"],shell=True)
-  #print("Result " , result)
+  print("Result " , result)
 
 def scheduling_forecast_weather():
   BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))

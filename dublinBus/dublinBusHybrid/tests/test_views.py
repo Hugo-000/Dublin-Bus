ad22c9c6ac6e7a889dc67cf8049f06df28769334
Jennifer Daly <jenniferdaly94@gmail.com>
2021-08-20 08:50:06 +0100

    updated the journeyPlanner test to take in a dictionary and a user to test the journeyPlanner page.
    RequestFactory from Django testing was used to create the test as it could create the request body with the data parameter and a request.user could be manually assigned.
    https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#django.test.RequestFactory

--- dublinBus/dublinBusHybrid/tests/test_views.py
+++ dublinBus/dublinBusHybrid/tests/test_views.py
@@ -1,10 +1,13 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase, Client
 from django.urls import resolve
 from dublinBusHybrid.views import JourneyPlanner, BusRoutes, Index
 from dublinBusHybrid.forms import JourneyPlannerForm
+from django.contrib.auth.models import AnonymousUser, User
 import datetime
 import os
 import sys
+
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -12,6 +15,13 @@ print("test base", BASE_DIR)
 
 
 class TestIndexView(TestCase):
+
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jamtd', email='jamtdsg1@gmail.com', password='DublinBus1')
+
     def test_resolve_to_index(self):
         """test the url works fine"""
         # resolve root path
@@ -49,9 +59,10 @@ class TestIndexView(TestCase):
 
     def test_get_directions(self):
         """test if the direction can e inferenced by lat and lng data: "UCD","UCD","2021-08-06","16:00"""
-        response = self.client.post("/dublinBusHybrid/journeyPlanner/",
-                                    {'origin_location': 'UCD', 'destination_location': 'Rathmines',
-                                     'travel_date': '2021-08-08', 'travel_time': '16:00'},
+        response = self.client.post("/dublinBusHybrid/journeyPlanner/", {
+                                        'origin_location': 'UCD', 'destination_location': 'Rathmines',
+                                        'travel_date': '2021-08-08', 'travel_time': '16:00'
+                                        },
                                     HTTP_ACCEPT='application/json')
         # # content_type  = "application/json"
         # print(response.content)
@@ -79,89 +90,92 @@ class TestIndexView(TestCase):
 
     def test_jouurney_planner(self):
         """test data from the post method can successfully predict the query without transfer"""
-        request = {'user':{'Error': 'User is not authenticated'},
+        requestBody = {
                     'travel_date': datetime.date(2021, 8, 12),
-                   'travel_time': datetime.time(21, 0),
-                   'Steps': [{
-                       'distance': {
-                           'text': '54 m',
-                           'value': 54
-                       },
-                       'duration': {
-                           'text': '1 min',
-                           'value': 38
-                       },
-                       'Instructions': 'Walk to Coolock Drive, stop 1249',
-                       'travel_mode': 'WALKING'
-                   },
-                       {
-                           'distance': {
-                               'text': '8.2 km',
-                               'value': 8234
-                           },
-                           'duration': {
-                               'text': '26 mins',
-                               'value': 1561
-                           },
-                           'instructions': 'Bus towards Jobstown',
-                           'transit': {
-                               'arrival_stop': {
-                                   'location': {
-                                       'lat': 53.3441118,
-                                       'lng': -6.265506299999999
-                                   },
-                                   'name': 'Dame Street, stop 1934'
-                               },
-                               'arrival_time': {
-                                   'text': '21:27',
-                                   'time_zone': 'Europe/Dublin',
-                                   'value': '2021-08-12T20:27:10.000Z'
-                               },
-                               'departure_stop': {
-                                   'location': {
-                                       'lat': 53.395181,
-                                       'lng': -6.2012402
-                                   },
-                                   'name': 'Coolock Drive, stop 1249'
-                               },
-                               'departure_time': {
-                                   'text': '21:01',
-                                   'time_zone': 'Europe/Dublin',
-                                   'value': '2021-08-12T20:01:09.000Z'
-                               },
-                               'headsign': 'Jobstown',
-                               'line': {
-                                   'agencies': [{
-                                       'name': 'Dublin Bus',
-                                       'url': 'https://www.dublinbus.ie/'
-                                   }],
-                                   'color': '#f2ca36',
-                                   'name': 'Templeview Avenue - Fortunestown Road',
-                                   'short_name': '27',
-                                   'text_color': '#000000',
-                                   'vehicle': {
-                                       'icon': 'https://maps.gstatic.com/mapfiles/transit/iw2/6/bus2.png',
-                                       'name': 'Bus', 'type': 'BUS'
-                                   }
-                               },
-                               'num_stops': 27
-                           },
-                           'travel_mode': 'TRANSIT'
-                       },
-                       {
-                           'distance': {
-                               'text': '46 m',
-                               'value': 46
-                           },
-                           'duration': {
-                               'text': '1 min',
-                               'value': 35
-                           },
-                           'instructions': 'Walk to Dame St, Dublin, Ireland',
-                           'travel_mode': 'WALKING'
-                       }]
-                   }
-        j = JourneyPlanner()
-        prediction =j.post(request=request,user={'Error': 'User is not authenticated'})
-        self.assertTrue(isinstance(prediction, int))
+                    'travel_time': datetime.time(21, 0),
+                    'Steps': [{
+                        'distance': {
+                            'text': '54 m',
+                            'value': 54
+                        },
+                        'duration': {
+                            'text': '1 min',
+                            'value': 38
+                        },
+                        'Instructions': 'Walk to Coolock Drive, stop 1249',
+                        'travel_mode': 'WALKING'
+                    },
+                        {
+                            'distance': {
+                                'text': '8.2 km',
+                                'value': 8234
+                            },
+                            'duration': {
+                                'text': '26 mins',
+                                'value': 1561
+                            },
+                            'instructions': 'Bus towards Jobstown',
+                            'transit': {
+                                'arrival_stop': {
+                                    'location': {
+                                        'lat': 53.3441118,
+                                        'lng': -6.265506299999999
+                                    },
+                                    'name': 'Dame Street, stop 1934'
+                                },
+                                'arrival_time': {
+                                    'text': '21:27',
+                                    'time_zone': 'Europe/Dublin',
+                                    'value': '2021-08-12T20:27:10.000Z'
+                                },
+                                'departure_stop': {
+                                    'location': {
+                                        'lat': 53.395181,
+                                        'lng': -6.2012402
+                                    },
+                                    'name': 'Coolock Drive, stop 1249'
+                                },
+                                'departure_time': {
+                                    'text': '21:01',
+                                    'time_zone': 'Europe/Dublin',
+                                    'value': '2021-08-12T20:01:09.000Z'
+                                },
+                                'headsign': 'Jobstown',
+                                'line': {
+                                    'agencies': [{
+                                        'name': 'Dublin Bus',
+                                        'url': 'https://www.dublinbus.ie/'
+                                    }],
+                                    'color': '#f2ca36',
+                                    'name': 'Templeview Avenue - Fortunestown Road',
+                                    'short_name': '27',
+                                    'text_color': '#000000',
+                                    'vehicle': {
+                                        'icon': 'https://maps.gstatic.com/mapfiles/transit/iw2/6/bus2.png',
+                                        'name': 'Bus', 'type': 'BUS'
+                                    }
+                                },
+                                'num_stops': 27
+                            },
+                            'travel_mode': 'TRANSIT'
+                        },
+                        {
+                            'distance': {
+                                'text': '46 m',
+                                'value': 46
+                            },
+                            'duration': {
+                                'text': '1 min',
+                                'value': 35
+                            },
+                            'instructions': 'Walk to Dame St, Dublin, Ireland',
+                            'travel_mode': 'WALKING'
+                        }]
+                    }
+
+        request = self.factory.post("/dublinBusHybrid/journeyPlanner/", requestBody)
+        request.user = self.user
+        
+        response = JourneyPlanner.as_view()(request)
+        self.assertEqual(response.status_code, 200)
 
